b26b781d03f0f9620f22f956f9935ff3975d0247
254guru <otienooluda@gmail.com>
2024-03-04 21:15:41 +0000

    avoid variable overwriting inside the conditional block and utilize block-scoping

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // Using let for block-scoping
-    let task2 = false; // Using let for block-scoping
+    // No need to declare new variables here, just reassign the existing ones
+    // Removing the declaration and using assignment directly
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
