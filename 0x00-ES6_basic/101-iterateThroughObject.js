d57fde7fa676854f37bca4ff5f478eea70e372ff
254guru <otienooluda@gmail.com>
2024-03-05 06:52:52 +0000

    iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  for (const employee of reportWithIterator) {
+    result += `${employee} | `;
+  }
+  // Remove the trailing ' | ' from the result
+  return result.slice(0, -3);
+}
