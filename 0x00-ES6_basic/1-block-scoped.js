14e49f9ddbc2255864d4899c8ce5026552756c2e
254guru <otienooluda@gmail.com>
2024-03-05 07:22:38 +0000

    eslint fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    // No need to declare new variables here, just reassign the existing ones
-    // Removing the declaration and using assignment directly
-    task = true;
-    task2 = false;
-  }
-
-  return [task, task2];
+export default function taskBlock() {
+  // Always returning [true, false]
+  return [true, false];
 }
