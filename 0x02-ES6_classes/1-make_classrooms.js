cdf319cf860453c2d5a89ed8532c1c5a5f420af3
254guru <otienooluda@gmail.com>
2024-03-07 14:10:31 +0000

    room sizes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-    const roomSizes = [19, 20, 34];
-    const rooms = roomSizes.map(size => new ClassRoom(size));
-    return rooms;
+  const roomSizes = [19, 20, 34];
+  const rooms = roomSizes.map((size) => new ClassRoom(size));
+  return rooms;
 }
 
 export default initializeRooms;
