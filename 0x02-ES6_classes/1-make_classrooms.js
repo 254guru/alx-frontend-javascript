201c8a34b1dee5d38604573cbb8e3d29118f7840
254guru <otienooluda@gmail.com>
2024-03-09 06:54:58 +0000

    make classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = roomSizes.map((size) => new ClassRoom(size));
-  return rooms;
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
-
-export default initializeRooms;
