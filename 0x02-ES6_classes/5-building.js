8a6fc9eab8413d75f184d77f55ac01b6d1e8b1a8
254guru <otienooluda@gmail.com>
2024-03-10 15:50:56 +0000

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,15 @@
 class Building {
-  constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Abstract classes cannot be instantiated directly');
+    constructor(sqft) {
+        this._sqft = sqft;
     }
-    this._sqft = sqft;
-  }
 
-  // Getter for sqft
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft() {
+        return this._sqft;
+    }
 
-  // Abstract method for evacuationWarningMessage
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
 }
 
 export default Building;
