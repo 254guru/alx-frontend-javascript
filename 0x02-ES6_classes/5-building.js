11b1397c74c8460dbb4601671c055558d947acdc
254guru <otienooluda@gmail.com>
2024-03-10 15:52:18 +0000

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,15 @@
 class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
-    }
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
 
 export default Building;
