4ced7ceaf319b9da86434821dcbb01c3d97e19ed
254guru <otienooluda@gmail.com>
2024-03-12 17:21:57 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
 function cleanSet(Set, startString) {
   let result = '';
-  for (const value of Set) {
+
+  Set.forEach((value) => {
     if (value.startsWith(startString)) {
       result += `${value.substring(startString.length)}-`;
     }
-  }
-  // Remove the trailing '-' if result is not empty
+  });
+
+  // Remove the trailing '-' if there are any elements
   if (result.length > 0) {
     result = result.slice(0, -1);
   }
+
   return result;
 }
 
