09fc681ccf9309b91b8740eac7483458d63dc0ed
254guru <otienooluda@gmail.com>
2024-03-12 17:16:24 +0000

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+function cleanSet(set, startString) {
+  const valuesArray = Array.from(set);
+  const filteredValues = valuesArray.filter((value) => value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  return cleanedValues.join('-');
+}
+
+export default cleanSet;
